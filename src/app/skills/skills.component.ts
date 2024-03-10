7d965ceaf17e48b36843fba68fe79cf0c2c7f637
remyakrish1495 <133562103+remyakrish1495@users.noreply.github.com>
2024-03-10 21:16:42 +0530

    Update skills.component.ts
    Changed according to star pattern

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -8,12 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class SkillsComponent implements OnInit {
 
   about1: any
-  
-
+  maxStars: number = 5
+  stars: number[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.stars = Array(this.maxStars).fill(0).map((_, i) => i + 1);
     this.about1 = `React, Angular, HTML, CSS, .NET Core, SQL Server`
   }
 
